56239339834ffafe6f65bd5f2b578e31b45e56e8
Fadi Chama <Shfad09501@hotmail.com>
2023-07-19 12:27:00 +0200

    ver 1.4 with Reflection

--- M.cs
+++ M.cs
@@ -9,45 +9,7 @@ namespace cars
         
         public string Color { get; set; }
         public double Price { get; set; }
-        //public M()
-        //{
-           
-        //}
-        //public void Add_data()
-        //{
-        //    if (File.Exists("m.txt"))
-        //    {
-        //        using (StreamWriter writer = new StreamWriter("m.txt",true))
-        //        {
-        //            writer.Write(Cars.Id + "-" + " " + this.Name + " " + this.Color + " " + this.Price + " "+"*");
-
-        //        }
-
-        //    }
-        //    else
-        //    {
-        //        StreamWriter writer = null;
-        //        try
-        //        {
-        //            writer = new StreamWriter("m.txt", true);
-
-        //            writer.Write(Cars.Id + "-" + " " + this.Name + " " + this.Color + " " + this.Price + " " + "*");
-
-
-
-        //        }
-        //        catch (IOException ex)
-        //        {
-        //            Console.WriteLine("Fehler beim Erstellen der Datei: " + ex.Message);
-        //        }
-        //        finally
-        //        {
-        //            writer?.Close();
-        //        }
-        //    }
-
-
-        //}
+        
         
 
     }
